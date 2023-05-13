4e46eafcd577d109a96cf859dea9d6e3773bf611
lbilinmis <leventamed@gmail.com>
2023-05-13 14:57:54 +0300

    builder.Services.AddCors(opt => {     //herhangi bir domainden tüm gelen istekleri kabul et anlamına gelir     //opt.AddDefaultPolicy(build =>     //{     //    build.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();     //});
    });

---
+++ Security.CORS.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Security.CORS.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
